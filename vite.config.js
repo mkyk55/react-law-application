b0e40fa2eb2c19f847d5d0580df3b9fef2820b46
Mayank Kumar <MKYK@ramboll.com>
2025-06-18 10:41:26 +0530

    Set up GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Replace with your GitHub repo name
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/react-law-application/', // ← VERY IMPORTANT
+});
